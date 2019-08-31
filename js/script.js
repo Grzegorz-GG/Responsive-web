e2aacb6ebdd95fb278b756734c20f45e32760671
Grzegorz <grzegorz_gasowski@wp.pl>
2019-08-31 11:19:10 +0200

    About carousel commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+
+//Preload
+
 $(window).on('load', function() {
     $('#status').fadeOut();
     $('#preloader').delay(400).fadeOut('slow');
+});
+
+//Carousel
+$('document').ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav:true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
 });
