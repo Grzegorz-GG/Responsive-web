9d9d61663e7ff99724bf491d7d70fdebcddfb693
Grzegorz <grzegorz_gasowski@wp.pl>
2019-09-03 00:46:08 +0200

    Stats section completed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-
 //Preload
 
-$(window).on('load', function() {
+$(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(400).fadeOut('slow');
 });
 
 //Team
-$('document').ready(function() {
+$('document').ready(function () {
     $('#team-members.owl-carousel').owlCarousel({
         items: 2,
         autoplay: true,
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true,
-        nav:true,
+        nav: true,
         dots: false,
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
     });
@@ -22,27 +21,29 @@ $('document').ready(function() {
 
 //Progress bar
 $(function () {
-        $(".progress-bar").each(function () {
-            $(this).animate({
-                width: $(this).attr("aria-valuenow") + "%"
-            }, 2000);
-        });
+    $(".progress-bar").each(function () {
+        $(this).animate({
+            width: $(this).attr("aria-valuenow") + "%"
+        }, 2000);
+    });
 });
 
 //Portfolio 
-$(window).on('load', function() {
+$(window).on('load', function () {
     let $plugin = $('#plugin-container').isotope();
 
-    $('#isotope-filters').on('click', 'button', function() {
+    $('#isotope-filters').on('click', 'button', function () {
         let filterValue = $(this).attr('data-filter');
-        $plugin.isotope({filter: filterValue});
+        $plugin.isotope({
+            filter: filterValue
+        });
 
         $('#isotope-filters').find('.active').removeClass('active');
         $(this).addClass('active');
     });
 });
 
-$('document').ready(function() {
+$('document').ready(function () {
     $('#plugin-container').magnificPopup({
         delegate: 'a',
         type: 'image',
@@ -53,15 +54,22 @@ $('document').ready(function() {
 });
 
 //Testimonials
-$('document').ready(function() {
+$('document').ready(function () {
     $('#testimonials-slider.owl-carousel').owlCarousel({
         items: 1,
         autoplay: false,
         smartSpeed: 700,
         loop: true,
-        nav:true,
+        nav: true,
         dots: false,
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
     });
 });
 
+//Stats
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2500,
+    });
+});
